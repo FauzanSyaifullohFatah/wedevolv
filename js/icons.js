3ac071184a5e428c234bb0ca4580926ae262d576
Fauzan Syaifulloh Fatah <fauzansyaifullohfatah@gmail.com>
2025-09-04 06:42:51 +0700

    Update icons.js

--- js/icons.js
+++ js/icons.js
@@ -1,25 +1,47 @@
-const svgElements = document.querySelectorAll("[class^='wd-']");
-        
-fetch("https://wedevolv.com/json/icons.json").then(res => res.json())
+let iconsData = [];
+
+fetch("json/icons.json").then(res => res.json())
 .then(data => {
-    svgElements.forEach(el => {
-        let text = el.innerHTML;
-        let styleElement = getComputedStyle(el);
-        el.style.display = "inline-flex"
+    iconsData = data;
+    renderIcons(document.querySelectorAll("[class^='wd-']"));
+    observeIcons();
+});
+
+function renderIcons(elements) {
+    elements.forEach(el => {
+        let text = el.textContent;
+        el.style.fontStyle = "normal";
+        el.style.display = "inline-flex";
         el.style.alignItems = "center";
         el.style.gap = "4px";
 
-        const className = el.classList[0];
-        const iconData = data.find(i => i.id === className);
+        const className = Array.from(el.classList).find(c => c.startsWith("wd-"));
+        const iconData = iconsData.find(i => i.id === className);
 
         if (iconData) {
             el.innerHTML = iconData.svg + " " + text;
+            let svg = el.querySelector("svg");
+
+            svg.setAttribute("fill", "currentColor");
+            svg.removeAttribute("width");
+            svg.removeAttribute("height");
+            svg.style.width = "1em";
+            svg.style.height = "1em";
         } else {
             el.innerHTML = `<small style="color:red">[Icon not found]</small>`;
         }
-        let svg = el.getElementsByTagName("svg")[0];
-        svg.style.width = styleElement.fontSize;
-        svg.style.fill = styleElement.fill;
-        svg.style.filter = `drop-shadow(${styleElement.textShadow})`;
     });
-});
+}
+
+function observeIcons() {
+    const observer = new MutationObserver(mutations => {
+        mutations.forEach(m => {
+            if (m.type === "attributes" && m.attributeName === "class") {
+                renderIcons([m.target]);
+            }
+        });
+    });
+    document.querySelectorAll("[class]").forEach(el => {
+        observer.observe(el, { attributes: true });
+    });
+}
